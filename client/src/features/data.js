f5268b35214794110fccd51f355057ebfcaac277
Stan-breaks <stanleymwendwa03@gmail.com>
2024-06-14 16:33:49 +0300

    long time no see

--- client/src/features/data.js
+++ client/src/features/data.js
@@ -13,5 +13,37 @@ export const locationOptions = [
 ];
 
 export const healthData = [
+  {
+    disease: "COVID-19",
+    trend: "Improving",
+    trendColor: "green",
+    prevalence: "12.5%",
+    mortalityRate: "2.1%",
+    recommendedTreatments: "Vaccination, Isolation",
+  },
+  {
+    disease: "Influenza",
+    trend: "Stable",
+    trendColor: "yellow",
+    prevalence: "8.2%",
+    mortalityRate: "0.5%",
+    recommendedTreatments: "Vaccination, Antiviral Drugs",
+  },
+  {
+    disease: "Diabetes",
+    trend: "Worsening",
+    trendColor: "red",
+    prevalence: "14.1%",
+    mortalityRate: "3.7%",
+    recommendedTreatments: "Lifestyle Changes, Medication, Insulin Therapy",
+  },
+  {
+    disease: "Heart Disease",
+    trend: "Worsening",
+    trendColor: "red",
+    prevalence: "11.8%",
+    mortalityRate: "4.2%",
+    recommendedTreatments: "Lifestyle Changes, Medication, Surgery",
+  },
   // Add more data objects as needed
 ];
