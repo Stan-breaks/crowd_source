896ad47ccef9758c5b360b30f1964cbf75a2167e
Stan-breaks <stanleymwendwa03@gmail.com>
2024-07-06 10:06:23 +0300

    setting up hook for get queries

--- client/src/features/profile/useProfile.ts
+++ client/src/features/profile/useProfile.ts
@@ -10,10 +10,25 @@ interface profileResponse {
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
-const fetchProfile = async () => {
+const fetchProfile = async (): Promise<profileResponse> => {
   const token = localStorage.getItem("token");
   if (!token) {
     throw new Error("No token found");
   }
-  const response = await fetch(`${apiUrl}/`);
+  const response = await fetch(`${apiUrl}/profile`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Error with fetching");
+  }
+  return response.json();
+};
+
+export const useProfile = () => {
+  return useQuery<profileResponse, Error>({
+    queryKey: ["profileData"],
+    queryFn: fetchProfile,
+  });
 };
