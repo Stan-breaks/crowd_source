40150635c9fbee4c8a094e6673d06d9b86eb2215
Stan-breaks <stanleymwendwa03@gmail.com>
2024-08-13 14:23:13 +0300

    removing about

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,13 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const Home = lazy(() => import("./pages/Home"));
-const About = lazy(()=> import("./pages/About"));
-const Landing = lazy(()=> import("./pages/Landing"));
-const Login = lazy(()=> import("./pages/Login"));
-const SignUp = lazy(()=> import("./pages/SignUp"));
-const Contact = lazy(()=> import("./pages/Contact"));
-const ForgotPassword = lazy(()=> import("./pages/ForgotPassword"));
-const NotFound = lazy(()=> import("./pages/NotFound"));
-const Loading = lazy(()=> import("./components/Loader"));
+const Landing = lazy(() => import("./pages/Landing"));
+const Login = lazy(() => import("./pages/Login"));
+const SignUp = lazy(() => import("./pages/SignUp"));
+const Contact = lazy(() => import("./pages/Contact"));
+const ForgotPassword = lazy(() => import("./pages/ForgotPassword"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const Loading = lazy(() => import("./components/Loader"));
 
 function App() {
   return (
@@ -20,7 +19,6 @@ function App() {
             <Route path="/Login" element={<Login />} />
             <Route path="/SignUp" element={<SignUp />} />
             <Route path="/Home" element={<Home />} />
-            <Route path="/About" element={<About />} />
             <Route path="/Contact" element={<Contact />} />
             <Route path="/ForgotPassword" element={<ForgotPassword />} />
             {/* Add a route for the NotFound component here for path are not registered */}
