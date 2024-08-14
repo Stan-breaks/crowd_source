e6e2617cfb8c4543ab6a017be358ce5bb2ac0d0e
Stan-breaks <stanleymwendwa03@gmail.com>
2024-08-14 13:27:17 +0300

    boilerplate for the usereport hook

---
+++ client/src/features/report/useReport.ts
@@ -0,0 +1,67 @@
+import {
+  QueryFunction,
+  UseMutationResult,
+  useMutation,
+  useQuery,
+} from "@tanstack/react-query";
+export interface PostResponse {
+  message: string;
+}
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+const fetchSettings: QueryFunction<
+  SettingsResponse,
+  [string, string]
+> = async ({ queryKey }) => {
+  const [key, userName] = queryKey;
+  const token = localStorage.getItem("token");
+  if (!token) {
+    throw new Error("token not found");
+  }
+  const response = await fetch(`${apiUrl}/profile/settings/${userName}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Error with fetching");
+  }
+  return response.json();
+};
+
+const postSettings = async (data: {
+  userName: string;
+  settings: SettingsResponse;
+}): Promise<PostResponse> => {
+  const token = localStorage.getItem("token");
+  const response = await fetch(`${apiUrl}/profile/settings/${data.userName}`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "content-Type": "application/json",
+    },
+    body: JSON.stringify(data.settings),
+  });
+  if (!response.ok) {
+    throw new Error("post failed");
+  }
+  return response.json();
+};
+
+export const usePostSettings = (): UseMutationResult<
+  PostResponse,
+  Error,
+  { userName: string; settings: SettingsResponse },
+  unknown
+> => {
+  return useMutation({
+    mutationFn: postSettings,
+  });
+};
+export const useGetSettings = (userName: string) => {
+  return useQuery({
+    queryKey: ["profileSettings", userName],
+    queryFn: fetchSettings,
+  });
+};
