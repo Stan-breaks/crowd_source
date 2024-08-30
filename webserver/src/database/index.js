2eaf08b2a2fc8021a4b257a8ba82166284e0437b
Stan-breaks <stanleymwendwa03@gmail.com>
2024-08-30 17:20:08 +0300

    uri

--- webserver/src/database/index.js
+++ webserver/src/database/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
+const dburi = process.env.DBURI;
 mongoose
-  .connect("mongodb://localhost:27017/crowdSource")
+  .connect(dburi)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
